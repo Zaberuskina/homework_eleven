33606bb846edfc1110a94773b75af9e88d846468
Zaberuskina, Anna <azaberuskina@prosv.ru>
2025-06-20 17:22:14 +0300

    добавление тестов

--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -1,18 +1,18 @@
 import allure
-from pages.registration_page import RegistrationPage
 from data.student_data import Student
-
+from pages.registration_page import RegistrationPage
 
 @allure.title("Успешная регистрация пользователя через UI")
-def test_submit_registration_form():
-    registration_page = RegistrationPage()
+def test_submit_registration_form(setup_browser):
     student = Student()
+    registration_page = RegistrationPage(setup_browser)
 
     with allure.step("Открыть страницу регистрации"):
         registration_page.open()
 
-    with allure.step("Заполнить форму"):
+    with allure.step("Заполнить форму студента"):
         registration_page.register(student)
 
     with allure.step("Проверить успешную регистрацию"):
         registration_page.should_have_registered(student)
+
